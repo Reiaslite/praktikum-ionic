037e2f05cb1d5de1daba86d09564df4e54d65703
Reiaslite <raidearaujo06@gmail.com>
2023-06-10 11:50:10 +0700

    push git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ const routes: Routes = [
     path:'',
     redirectTo:'home',
     pathMatch: 'full'
-  },
-  {
+  }
+  ,{
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
